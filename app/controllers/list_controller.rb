df3dcab5539cd12a29208de6517e59d65cd1fe40
EC2 Default User <ec2-user@ip-172-31-40-124.ap-northeast-1.compute.internal>
2020-03-21 09:44:35 +0000

    リスト編集機能と削除機能の実装

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,6 @@
 class ListController < ApplicationController
+  before_action :set_list, only: %i(edit update destroy)
+  
   def new
     @list = List.new
   end
@@ -12,8 +14,28 @@ class ListController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @list.update_attributes(list_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
+  end
+  
+  def destroy
+    @list.destroy
+    redirect_to :root
+  end
+  
   private
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)
+    end
+    
+    def set_list
+      @list = List.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   
-  resources :list, only: %i(new create edit)
+  resources :list, only: %i(new create edit update destroy)
 end
