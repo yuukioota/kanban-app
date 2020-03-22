841d3457d47bf59567d98578a47cfa4c77bd424c
EC2 Default User <ec2-user@ip-172-31-40-124.ap-northeast-1.compute.internal>
2020-03-22 11:57:13 +0000

    カード編集機能の実装

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,4 +1,6 @@
 class CardController < ApplicationController
+  before_action :set_card, only: %i(show edit update)
+  
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
@@ -14,11 +16,25 @@ class CardController < ApplicationController
   end
   
   def show
-    @card = Card.find_by(id: params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
   end
   
   private
     def card_params
       params.require(:card).permit(:title, :memo, :list_id)
+    end
+    
+    def set_card
+      @card = Card.find_by(id: params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'top#index'
   
   resources :list, only: %i(new create edit update destroy) do
-    resources :card, only: %i(new create show edit)
+    resources :card, only: %i(new create show edit update)
   end
 end
