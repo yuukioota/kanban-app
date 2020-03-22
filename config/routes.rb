b01899c2f3707d9b2a2346b7813a5a1d47db4abd
EC2 Default User <ec2-user@ip-172-31-40-124.ap-northeast-1.compute.internal>
2020-03-22 11:29:16 +0000

    カード作成機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   
-  resources :list, only: %i(new create edit update destroy)
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create)
+  end
 end
